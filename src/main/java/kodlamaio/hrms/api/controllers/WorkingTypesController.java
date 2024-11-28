95ec4703e2abf91317cf8848162c2c4eb5b42a3d
Halil İbrahim Kavalcı <110342285+IbrahimKvlci@users.noreply.github.com>
2024-11-28 20:01:16 +0300

    Added City, WorkingTime, WorkingType Services

---
+++ src/main/java/kodlamaio/hrms/api/controllers/WorkingTypesController.java
@@ -0,0 +1,32 @@
+package kodlamaio.hrms.api.controllers;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import kodlamaio.hrms.business.abstracts.WorkingTypeService;
+import kodlamaio.hrms.core.utilities.results.DataResult;
+import kodlamaio.hrms.entities.concretes.WorkingType;
+
+@RestController
+@RequestMapping("/api/workingTypes")
+@CrossOrigin
+public class WorkingTypesController {
+
+	private WorkingTypeService workingTypeService;
+
+	@Autowired
+	public WorkingTypesController(WorkingTypeService workingTypeService) {
+		super();
+		this.workingTypeService = workingTypeService;
+	}
+	
+	@GetMapping("/getAll")
+	public DataResult<List<WorkingType>> getAll(){
+		return this.workingTypeService.getAll();
+	}
+}
